f050830d88cfd6275bc0af67bbf5a4bd36d38337
Tabish Rashidi <tabish.rashidi@gmail.com>
2022-05-20 11:43:44 -0400

    Adding in TypeScript learning modules

--- tsPractice/lesson6.ts
+++ tsPractice/lesson6.ts
@@ -5,6 +5,8 @@ interface GameObject1 {
     x: number;
     y: number;
     z: number;
+
+    update(): void; 
 } 
 
 // Classes - what data you want objects to hold plus methods
@@ -12,7 +14,8 @@ interface GameObject1 {
 // Game of players
 // Fields are public by default 
 // Classes must conform to interface
-class Player3 implements GameObject1 {
+// Abstract class
+abstract class Player3 implements GameObject1 {
 
     static playerCount = 0; // Used to store constants across all instances of the class or some shared data between all classes
 
@@ -30,10 +33,48 @@ class Player3 implements GameObject1 {
     display(prefix: string) {
         console.log(this.name, this.x, this.y)
     } 
+
+    update() {
+        // Move player
+    }
+
+    // Child classes must implement this 
+    abstract getConnection(): void; 
+
+    // Kept private to make sure no one else outside of class can access that
+    protected calculateMotion() {
+
+    }
 }
 
+class Gunner extends Player3 {
+    // Gunner inherits anything + the following 
+    bullets = 0; 
+
+    constructor(
+        name: string, 
+        public gunType: string
+    ) {
+        super(name);
+    }
+
+    update() {
+        super.update(); // Call parent update to move player
+        this.bullets++;
+        this.calculateMotion(); 
+    }
+
+
+    getConnection() {
+        // Get ports, connect etc....
+    }
+
+
+}
+
+
 // instantiation
-let p2 = new Player3("Tyler");
-let p3 = new Player3("Fred");
+let p2 = new Gunner("Tyler", "SMG");
+let p3 = new Gunner("Fred", "SubmachineGun");
 
 let objects1: GameObject1[] = [p2, p3]; 
